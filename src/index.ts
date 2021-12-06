0f84caa8a1e34418c86d41ab7ed32c78765a7cf2
Marcos Wergles <marcospsw96@gmail.com>
2021-12-05 22:06:58 -0300

    finalizando desafio 05

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import express from "express";
-
-import { usersRoutes } from "./routes/users.routes";
+import express from 'express';
+import { usersRoutes } from './routes/users.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/users", usersRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/users', usersRoutes);
 
 export { app };
